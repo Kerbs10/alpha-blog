011f2a0797a3f9ead3e5b09e6662cbd7e29c16ec
Rails Course <npdannug17@gmail.com>
2018-07-27 14:28:26 +0000

    restrict user actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,7 +26,7 @@ class ArticlesController < ApplicationController
   
   def create
     @article = Article.new(article_params)
-    
+    @article.user = current_user
     if @article.save
       flash[:success] = "The article was successfully created."
       redirect_to article_path(@article)
